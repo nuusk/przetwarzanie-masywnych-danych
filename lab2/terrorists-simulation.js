102105cce7459bcc5ab5af1095921e48e41bd944
poe <piotr.a.ptak@icloud.com>
2018-10-25 14:23:20 +0200

    Add many numbers of simulations and print average results

--- lab2/terrorists-simulation.js
+++ lab2/terrorists-simulation.js
@@ -1,56 +1,7 @@
 const PERFORMANCE_MODE = true;
 
 // The number of people in the simulation
-const numPeople = 10000;
-
-// The probability of staying in the hotel (for each person)
-const hotelSleepProb = 0.1;
-
-// The number of hotels
-const numHotels = 100;
-
-// The number of days in the simulation
-const numDays = 100;
-
-let currentDay = 0;
-let DEBUG_totalHotelNights = 0;
-
-// hashmap "A B": x, where A and B are people's ID, and x is the number of meetings in the same hotel
-let matches = {};
-
-// Just a set of suspects (since one suspect can be in a pair with many diffrent people)
-let suspects = new Set();
-
-// Number of pairs
-let numPairs = 0;
-
-let countMatchingSuspectsAndHotel = 0;
-
-function matchPeople(personA, personB) {
-  // Additional functionality to make sure matching is sorted (so always A is lower than B)
-  let A = personA;
-  let B = personB;
-  
-  if (!PERFORMANCE_MODE) {
-    A = Math.min(personA, personB);
-    B = A === personA ? personB : personA;
-  }
-
-  const matchKey = `${A} ${B}`;
-  // console.log(`[${currentDay}] new Match`, matchKey);
-
-  if (!matches[matchKey]) {
-    matches[matchKey] = 1;
-  } else {
-    matches[matchKey]++;
-  }
-
-  // Adding new suspects
-  if (matches[matchKey] >= 2) {
-    suspects.add(A);
-    suspects.add(B);
-  }
-}
+const NUM_SIMULATIONS = 3;
 
 function combination(n, k) {
   let tmp = 1;
@@ -92,81 +43,183 @@ class Hotel {
   }
 }
 
-let people = [];
-for (let i=0; i<numPeople; i++) {
-  people.push(new Person(i));
-}
-let hotels = [];
-for (let i=0; i<numHotels; i++) {
-  hotels.push(new Hotel(i, numDays));
-}
+class Simulation {
+  constructor(numPeople, hotelSleepProb, numHotels, numDays) {
+    this.numPeople = numPeople;
+    this.hotelSleepProb = hotelSleepProb;
+    this.numHotels = numHotels;
+    this.numDays  = numDays;
 
-while(currentDay < numDays) {
-  console.log(`~~ Starting day number ${currentDay}. ~~`);
+    this.SIMULATIONS_histogram = [];
+    this.SIMULATIONS_numPairs = [];
+    this.SIMULATIONS_countMatchingSuspectsAndHotel = [];
+    this.SIMULATIONS_numSuspects = [];
 
-  people.forEach((person, personIndex) => {
-    const tmpProb = Math.random();
-    if (tmpProb < hotelSleepProb) {
-      // This person is staying at the hotel. Now he randomly choses one of the hotels
-      const randHotel = Math.floor(Math.random()*numHotels);
-      
-      hotels[randHotel].hostPerson(currentDay, personIndex);
+    this.simulationNumber = 0;
 
-      DEBUG_totalHotelNights++;
-    }
-  });
+    this.matches = {};
 
-  hotels.forEach((hotel, hotelIndex) => {
-    // Sort visitors so later we can arrenge meetings with ascending order, preventing the need of duplicating meetings for each pair of people (since Person A ID will always be lower than Person B ID)
-    hotel.sortVisitors(currentDay);
-
-    // console.log('~~~~ analysing hotel' ,hotelIndex /*,  hotel.visitors[currentDay]*/ );
-    for (let i=0; i<hotel.visitors[currentDay].length; i++) {
-      for(let j=i+1; j<hotel.visitors[currentDay].length; j++) {
-        matchPeople(hotel.visitors[currentDay][i], hotel.visitors[currentDay][j]);
-      }
-    }
-    hotel.visitors[currentDay].forEach(visitor => {
-
-    });
-  });
-
-  // console.log(matches);
-
-  currentDay++;
-}
-
-// console.log(`[DEBUG] Overall, people stayed at hotel ${DEBUG_totalHotelNights} times during all nights combined.`);
-// console.log(`[DEBUG] That is ${DEBUG_totalHotelNights/(numPeople*numDays)} percentage, compare it with ${hotelSleepProb}.`);
-
-// console.log(`Final matching hashmap`);
-// console.log(matches);
-
-let histogram = [];
-for (i in matches) {
-  if (!histogram[matches[i]]) {
-    histogram[matches[i]] = 0;
+    this.resetSimulation();
   }
-  histogram[matches[i]]++;
+
+  resetSimulation() {
+    this.people = [];
+    for (let i=0; i<this.numPeople; i++) {
+      this.people.push(new Person());
+    }
+
+    this.hotels = [];
+    for (let i=0; i<this.numHotels; i++) {
+      this.hotels.push(new Hotel(i, this.numDays));
+    }
+
+    this.currentDay = 0;
+  }
+
+  matchPeople(personA, personB) {
+    // Additional functionality to make sure matching is sorted (so always A is lower than B)
+    let A = personA;
+    let B = personB;
+    
+    if (!PERFORMANCE_MODE) {
+      A = Math.min(personA, personB);
+      B = A === personA ? personB : personA;
+    }
+  
+    const matchKey = `${A} ${B}`;
+    // console.log(`[${this.currentDay}] new Match`, matchKey);
+  
+    if (!this.matches[matchKey]) {
+      this.matches[matchKey] = 1;
+    } else {
+      this.matches[matchKey]++;
+    }
+  
+    // Adding new suspects
+    if (this.matches[matchKey] >= 2) {
+      this.suspects.add(A);
+      this.suspects.add(B);
+    }
+  }
+
+  run() {
+    let currentSimulationHistogram = [];
+
+    // hashmap "A B": x, where A and B are people's ID, and x is the number of meetings in the same hotel
+    this.matches = {};
+
+    // Just a set of suspects (since one suspect can be in a pair with many diffrent people)
+    this.suspects = new Set();
+
+    // Number of pairs
+    let numPairs = 0;
+
+    // Count 
+    let countMatchingSuspectsAndHotel = 0;
+
+    while(this.currentDay < this.numDays) {
+      // console.log(`~~ Starting day number ${this.currentDay}. ~~`);
+
+      this.people.forEach((person, personIndex) => {
+        const tmpProb = Math.random();
+        if (tmpProb < this.hotelSleepProb) {
+          // This person is staying at the hotel. Now he randomly choses one of the hotels
+          const randHotel = Math.floor(Math.random() * this.numHotels);
+          
+          this.hotels[randHotel].hostPerson(this.currentDay, personIndex);
+        }
+      });
+
+      this.hotels.forEach((hotel, hotelIndex) => {
+        // Sort visitors so later we can arrenge meetings with ascending order, preventing the need of duplicating meetings for each pair of people (since Person A ID will always be lower than Person B ID)
+        hotel.sortVisitors(this.currentDay);
+    
+        // console.log('~~~~ analysing hotel' ,hotelIndex /*,  hotel.visitors[currentDay]*/ );
+        for (let i=0; i<hotel.visitors[this.currentDay].length; i++) {
+          for(let j=i+1; j<hotel.visitors[this.currentDay].length; j++) {
+            this.matchPeople(hotel.visitors[this.currentDay][i], hotel.visitors[this.currentDay][j]);
+          }
+        }
+      });
+
+      this.currentDay++;
+    }
+
+    for (let matchKey in this.matches) {
+      if (!currentSimulationHistogram[this.matches[matchKey]]) {
+        currentSimulationHistogram[this.matches[matchKey]] = 0;
+      }
+      currentSimulationHistogram[this.matches[matchKey]]++;
+    }
+
+    // Counting the pairs and matching suspects & pairs
+    for (let meetingsNumber = 2; meetingsNumber < currentSimulationHistogram.length; meetingsNumber++) {
+      numPairs += currentSimulationHistogram[meetingsNumber];
+    }
+
+    // Counting pairs&days, which is the number of combination of pairs
+    for (let meetingsNumber = 2; meetingsNumber < currentSimulationHistogram.length; meetingsNumber++) {
+      countMatchingSuspectsAndHotel += combination(meetingsNumber, 2) * currentSimulationHistogram[meetingsNumber];
+    }
+
+    this.SIMULATIONS_histogram.push(currentSimulationHistogram);
+    this.SIMULATIONS_numPairs.push(numPairs);
+    this.SIMULATIONS_countMatchingSuspectsAndHotel.push(countMatchingSuspectsAndHotel);
+    this.SIMULATIONS_numSuspects.push(this.suspects.size);
+
+    this.simulationNumber++;
+  }
+
+  printSimulationResults(simulationNumber) {
+    // The answer is written in polish because the classes are also in polish
+    console.log(`\n~~~ WYNIKI SYMULACJI nr ${simulationNumber} ~~~ `);
+
+    console.log('Histogram: ');
+    for (let i = 1; i < this.SIMULATIONS_histogram[simulationNumber].length; i++) {
+      console.log(`[${i+1}] -> ${this.SIMULATIONS_histogram[simulationNumber][i]}`);
+    }
+    console.log(`Podejrzanych par jest ${this.SIMULATIONS_numPairs[simulationNumber]}`);
+    console.log(`Licznik "par i dni" wynosi ${this.SIMULATIONS_countMatchingSuspectsAndHotel[simulationNumber]}`);
+    console.log(`Wszystkich podejrzanych jest łącznie ${this.SIMULATIONS_numSuspects[simulationNumber]}\n`);
+  }
+
+  printAverageResults(numSimulations) {
+    let averageHistogram = [];
+    for (let i = 0; i <= numSimulations; i++) {
+      averageHistogram[i] = 0;
+    }
+    let averageNumPairs = 0;
+    let averageCountPairsAndDays = 0;
+    let averageNumSuspects = 0;
+
+    for (let i = 0; i < numSimulations; i++) {
+      for (let j = 1; j < this.SIMULATIONS_histogram[i].length; j++) {
+        averageHistogram[j] += this.SIMULATIONS_histogram[i][j];
+      }
+
+      averageNumPairs += this.SIMULATIONS_numPairs[i];
+      averageCountPairsAndDays += this.SIMULATIONS_countMatchingSuspectsAndHotel[i];
+      averageNumSuspects += this.SIMULATIONS_numSuspects[i];
+    }
+
+    console.log(`\n~~~ ŚREDNIE WYNIKI SYMULACJI ~~~ `);
+    console.log('Średni histogram: ');
+    for (let i = 1; i < averageHistogram.length; i++) {
+      console.log(`[${i+1}] -> ${averageHistogram[i]/numSimulations}`);
+    }
+    console.log(`Średnio podejrzanych par jest ${Math.round(100 * averageNumPairs/numSimulations) / 100}`);
+    console.log(`Średnio licznik "par i dni" wynosi ${Math.round(100 * averageCountPairsAndDays/numSimulations) / 100}`);
+    console.log(`Średnio wszystkich podejrzanych jest łącznie ${Math.round(100 * averageNumSuspects/numSimulations) / 100}\n`);
+  }
 }
 
-// Counting the pairs and matching suspects & pairs
-for (let meetingsNumber = 2; meetingsNumber < histogram.length; meetingsNumber++) {
-  numPairs += histogram[meetingsNumber];
+// (numPeople, probability, numHotels, numDays)
+const app = new Simulation(10000, 0.1, 100, 100);
+
+for (let i=0; i<NUM_SIMULATIONS; i++) {
+  app.run();
+  app.printSimulationResults(i);
+  app.resetSimulation();
 }
 
-// for (let i=2; i<histogram.length; i++) {
-//   histogram[i]
-// }
-
-// Counting pairs&days, which is the number of combination of pairs
-for (let meetingsNumber = 2; meetingsNumber < histogram.length; meetingsNumber++) {
-  countMatchingSuspectsAndHotel += combination(meetingsNumber, 2) * histogram[meetingsNumber];
-}
-
-console.log(histogram);
-
-// The answer is written in polish because the classes are also in polish
-console.log(`Podejrzanych par jest ${numPairs}`);
-console.log(`Licznik "par i dni" wynosi ${countMatchingSuspectsAndHotel}`);
-console.log(`Wszystkich podejrzanych jest łącznie ${suspects.size}`);
+app.printAverageResults(NUM_SIMULATIONS);
