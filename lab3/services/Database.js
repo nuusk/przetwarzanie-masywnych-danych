612d901f9a72879c4a00900d5e9648a960928aff
Poe <piotr.a.ptak@icloud.com>
2018-11-08 00:11:27 +0100

    [03] Working psql :contruction

--- lab3/services/Database.js
+++ lab3/services/Database.js
@@ -3,15 +3,32 @@ require('dotenv').config();
 
 class Database {
   constructor() {
-    this.client = new pg.Client('postgres://poe:NIEpodam123@db:5432');
+    this.client = new pg.Client({
+      // 'postgres://poe:NIEpodam123@million-song:5432'
+      user: 'poe',
+      host: 'db',
+      database: 'million-song',
+      password: 'NIEpodam123',
+      port: 5432,
+    });
     this.client.connect().then(() => {
+      console.log('Successfully connected to db...');
+
       this.client.query(
         'create table if not exists TRACKS (TRACK_ID VARCHAR(32), RECORDING_ID VARCHAR(32), ARTIST_NAME VARCHAR(32), TRACK_NAME VARCHAR(32))'
-      );
+      ).then(res => {
+        // console.log(res);
+      }).catch(err => {
+        throw err;
+      });
       
       this.client.query(
         'create table if not exists LISTEN_ACTIVITIES (TRACK_ID VARCHAR(32), USER_ID VARCHAR(32), ACTIVITY_DATE DATE)'
-      );
+      ).then(res => {
+        // console.log(res);
+      }).catch(err => {
+        throw err;
+      });
     });
   }
 
